83bf185d4c00dbf8f4d0a61676ccabaf751236c7
Allysson Alves <allissonalvesvjt@hotmail.com>
2025-10-21 00:48:50 -0300

    Adicionando novo arquivo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FocoVest/', // 👈 Adicione esta linha
   plugins: [react()],
   resolve: {
     alias: {
